9a767b3e677a2387cf8017538bdb7746d4ec04bb
ayush20892 <ayush20892@gmail.com>
2021-05-13 17:02:22 +0530

    feat: setting up express enviroment

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const { errorHandler } = require('./middlewares/errorHandler.js')
+const { routeNotFound } = require('./middlewares/routeNotFound.js')
+
+app.get('/', (req,res) => {
+  res.send({ "API" : "Unusual" })
+})
+
+
+app.use(routeNotFound)
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}!`)
+})
